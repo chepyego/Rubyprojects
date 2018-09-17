ff0a0c75750978d921c4737ca1b6d869c2283693
chepyego <jchepyego@gmail.com>
2018-09-18 00:15:38 +0300

    conversion of celsius to farenheit

---
+++ C2F.rb
@@ -0,0 +1,15 @@
+# celsius = 100
+# fahrenheit = (celsius * 9 / 5) +32
+# puts "The result is: "
+
+# puts fahrenheit
+
+# puts "."
+
+celsius = 100
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+puts "Your results is #{fahrenheit}"
+
+puts "."
