a22d3fccc1eb9fb6f385cb5613e581f9fe388430
chepyego <jchepyego@gmail.com>
2018-09-23 23:52:13 +0300

    llocal variables

---
+++ p.rb
@@ -0,0 +1,19 @@
+# puts "Please enter your password:"
+
+#  password = gets.chomp
+
+#  if password == "jemimah"
+
+#     puts  true
+#  else
+#    puts  false
+#  end
+
+
+# Local  variables scope   is limited to where it is declared
+ 10.times do 
+
+    x = 10
+    p  x 
+ end
+ 
